5c5604aa2754674263b360e285d8eecf66e9e9a3
Mauricio <mauriciodiego2019@gmail.com>
2023-05-26 10:11:54 -0500

    finish

--- calculadora.py
+++ calculadora.py
@@ -21,15 +21,12 @@ def btn_equal():
     except:
         entry.delete(0, tk.END)
         entry.insert(tk.END, "Error")
-# Crear la ventana
 window = tk.Tk()
 window.title("Calculadora LAB10 EDYA")
 
-# Crear la caja de entrada
 entry = tk.Entry(window, width=20)
 entry.grid(row=0, column=0, columnspan=4)
 
-# Crear los botones numéricos
 btn_1 = tk.Button(window, text="1", command=lambda: btn_click(1))
 btn_1.grid(row=1, column=0)
 btn_2 = tk.Button(window, text="2", command=lambda: btn_click(2))
@@ -48,9 +45,7 @@ btn_8 = tk.Button(window, text="8", command=lambda: btn_click(8))
 btn_8.grid(row=3, column=1)
 btn_9 = tk.Button(window, text="9", command=lambda: btn_click(9))
 btn_9.grid(row=3, column=2)
-# ... Crear los demás botones numéricos
 
-# Crear los botones de operaciones
 btn_plus = tk.Button(window, text="+", command=lambda: btn_click("+"))
 btn_plus.grid(row=4, column=0)
 btn_minus = tk.Button(window, text="-", command=lambda: btn_click("-"))
@@ -65,15 +60,13 @@ btn_par1 = tk.Button(window, text="(", command=lambda: btn_click("("))
 btn_par1.grid(row=6, column=0)
 btn_par2 = tk.Button(window, text=")", command=lambda: btn_click(")"))
 btn_par2.grid(row=6, column=1)
-# ... Crear los demás botones de operaciones
 
-# Crear el botón de igual
 btn_equal = tk.Button(window, text="=", command=btn_equal)
 btn_equal.grid(row=5, column=2, columnspan=1)
 
-# Crear el botón de limpiar
+
 btn_clear = tk.Button(window, text="C", command=btn_clear)
 btn_clear.grid(row=7, column=1, columnspan=1)
 
-# Iniciar el bucle principal de la ventana
+
 window.mainloop()
